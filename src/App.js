6806dee760c3913a30e12e7e4343274828f02265
ZuzanaElt <zuzanaeltom@gmail.com>
2023-03-13 21:14:14 +0000

    testing deploying

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import ProjectsGallery from "./pages/ProjectsGallery/ProjectsGallery";
-//import ProjectCardTemplate from "./components/ProjectCardTemplate/ProjectCardTemplate";
 import Footer from "./components/Footer/Footer";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Header from "./components/Header/Header";
