86b26459afea636b73b72f5d537a7a0e782062e7
ZuzanaElt <zuzanaeltom@gmail.com>
2024-02-07 16:53:12 +0100

    amending pdf size

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -7,8 +7,8 @@ import pdf2 from '../assets/images/ZuzanaKnightovaFeb2024ENG.pdf';
 function Resume() {
     return (
       <div>
-        <object width="100%" height="600" data={pdf1} type="application/pdf">   </object>
-        <object width="100%" height="600" data={pdf2} type="application/pdf">   </object>
+        <object width="100%" height="500" data={pdf1} type="application/pdf">   </object>
+        <object width="100%" height="500" data={pdf2} type="application/pdf">   </object>
 
         
       </div>
